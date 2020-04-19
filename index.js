c323912b006ed32ae2a012679ea3b517a416b3c8
Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-04-18 21:09:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+main.remove()
+
+// Flatiron Solution:
+// document.querySelector("main#main").remove();
+
+let newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+newHeader.innerHTML = "Uzoma is the champion"
+
+// Flatiron Solution:
+
+// const newHeader = document.createElement("h1");
+// newHeader.id = "victory"
+// newHeader.innerHTML = "Flatiron School is the champion!";
+// newHeader.className = "victory";
