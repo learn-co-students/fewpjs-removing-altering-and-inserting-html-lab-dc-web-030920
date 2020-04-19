da5296fd9b6475b39efb01b085fc8f135f41398d
github username <github email address>
2020-04-18 23:44:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let main = document.getElementById('main');
+main.remove();
+const newHeader = document.createElement('H1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+
